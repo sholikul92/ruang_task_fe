3f488ab30e74dd57eece11c9f0310dcd40a87601
sholikul <sholikulardian@gmail.com>
2024-12-12 21:30:49 +0700

    feat: added component chat

---
+++ src/components/ui/chat/Chat.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { ChatBubble } from "./ChatBubble";
+
+interface Props {
+  message: string;
+  type: string;
+}
+
+export const Chat: React.FC<Props> = ({ message, type }) => {
+  return type === "question" ? (
+    <ChatBubble className='bg-primary text-white rounded-bl-xl self-end' message={message} />
+  ) : (
+    <ChatBubble className='bg-secondary text-black rounded-br-xl' message={message} />
+  );
+};
