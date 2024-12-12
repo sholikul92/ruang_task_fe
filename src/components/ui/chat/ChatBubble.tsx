20367914879942add844bcc9ca62bd4ccb0d9bbc
sholikul <sholikulardian@gmail.com>
2024-12-12 21:30:34 +0700

    fix: refactor code

--- src/components/ui/chat/ChatBubble.tsx
+++ src/components/ui/chat/ChatBubble.tsx
@@ -1,14 +1,10 @@
 import React from "react";
 
-interface Props {
-  message: string;
-  type: string;
+interface Content {
+  className: string;
+  message: string | JSX.Element | null;
 }
 
-export const ChatBubble: React.FC<Props> = ({ message, type }) => {
-  return type === "question" ? (
-    <div className='bg-primary max-w-96 w-max p-2 text-white rounded-tr-xl rounded-tl-xl rounded-bl-xl self-end'>{message}</div>
-  ) : (
-    <div className='bg-secondary max-w-96 w-max p-2 text-black rounded-tr-xl rounded-tl-xl rounded-br-xl '>{message}</div>
-  );
+export const ChatBubble: React.FC<Content> = ({ className, message }) => {
+  return <div className={`${className} max-w-96 w-max p-2 rounded-tr-xl rounded-tl-xl`}>{message}</div>;
 };
