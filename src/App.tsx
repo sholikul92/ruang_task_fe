bf64157256bc3d566c479fd688cc9a0b23236236
sholikul <sholikulardian@gmail.com>
2024-12-13 13:36:09 +0700

    fix: change input from tag input become text area

--- src/App.tsx
+++ src/App.tsx
@@ -18,9 +18,11 @@ export const App: React.FC = () => {
   const [chats, setChats] = useState<Array<Chats>>([]);
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<boolean>(false);
+  const textAreaRef = useRef<HTMLTextAreaElement | null>(null);
 
   const handleAttachmentClick = () => {
     if (fileInputRef.current) {
+      fileInputRef.current.value = "";
       fileInputRef.current.click();
     }
   };
@@ -28,13 +30,30 @@ export const App: React.FC = () => {
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files && e.target.files.length > 0) {
       setFile(e.target.files[0]);
+      console.log(file);
+
+      if (fileInputRef.current) {
+        fileInputRef.current.value = "";
+      }
     } else {
       setFile(null);
+      console.log(file);
     }
   };
 
-  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleInput = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setQuery(e.target.value);
+
+    const textarea = textAreaRef.current;
+    if (textarea) {
+      textarea.style.height = "auto";
+
+      const maxRow = 5;
+      const rowHeight = 24;
+      const maxHeight = maxRow * rowHeight;
+
+      textarea.style.height = `${Math.min(textarea.scrollHeight, maxHeight)}px`;
+    }
   };
 
   const handleForm = (e: React.FormEvent<HTMLFormElement>) => {
@@ -120,6 +139,7 @@ export const App: React.FC = () => {
     } catch (err) {
       if (err instanceof AxiosError) {
         console.log(err);
+        setError(true);
       }
     } finally {
       setLoading(false);
@@ -127,28 +147,28 @@ export const App: React.FC = () => {
   };
 
   return (
-    <div className='min-h-screen flex justify-center items-center'>
-      <div className='p-4 rounded-xl border-2 h-[90vh] flex flex-col gap-2 min-w-[70vw]'>
-        <div>
-          <div className='flex'>
-            <h1 className='font-bold text-4xl'>
-              Ruang<span className='text-primary'>Chat</span>
-            </h1>
-            <img src='logo.svg' width={50} />
-          </div>
-          <hr className='min-w-full border mt-2' />
-        </div>
-        <ScrollArea className='px-4 flex-1 '>
+    <div className='rounded-xl h-[95vh] flex flex-col gap-2 min-w-[70vw] w-full mr-8 mt-4'>
+      <div className='flex justify-center md:justify-normal'>
+        <h1 className='font-bold text-4xl'>
+          Ruang<span className='text-primary'>Chat</span>
+        </h1>
+        <img src='logo.svg' width={50} />
+      </div>
+      <div className='flex flex-col flex-1 md:w-[60%] self-center h-[90%] '>
+        <ScrollArea className='px-4 flex-1'>
           <div className='flex flex-col gap-2'>
             {chats && chats.map((chat, index) => <Chat message={chat.message} type={chat.type} key={index} />)}
           </div>
           {loading && <ChatBubble className='bg-secondary text-black rounded-br-xl' message={<PulseLoader size={5} />} />}
           {error && (
-            <ChatBubble className='bg-secondary text-black rounded-br-xl' message='Sorry, there is a problem at the moment, please try again 😭' />
+            <ChatBubble
+              className='bg-secondary text-black rounded-br-xl mt-2'
+              message='Sorry, there is a problem at the moment, please try again 😭'
+            />
           )}
         </ScrollArea>
 
-        <form method='POST' onSubmit={handleForm} className='flex min-w-full max-w-sm items-center space-x-1'>
+        <form method='POST' onSubmit={handleForm} className='flex mx-4 space-x-1 border-2 rounded-xl p-2 items-end mt-2'>
           <button
             type='button'
             className='flex items-center justify-center w-10 h-10 rounded-full hover:text-gray-600 text-gray-400'
@@ -159,15 +179,24 @@ export const App: React.FC = () => {
           </button>
 
           <input type='file' ref={fileInputRef} style={{ display: "none" }} aria-label='File input' onChange={handleFileChange} />
-          <div className='flex flex-col border p-2 rounded-xl w-full'>
+          <div className='flex flex-col w-full'>
             {file && (
               <div className='bg-gray-200 flex w-max p-2 mb-4'>
                 <FileIcon /> {file?.name}
               </div>
             )}
-            <input type='text' placeholder='Input your message...' value={query} className='focus:outline-none' onChange={handleInput} />
+            <textarea
+              ref={textAreaRef}
+              placeholder='Input your message...'
+              value={query}
+              rows={2}
+              cols={24}
+              style={{ lineHeight: "1.2" }}
+              className='focus:outline-none w-full resize-none overflow-auto'
+              onChange={handleInput}
+            />
           </div>
-          <button type='submit' className='rounded-full p-2.5'>
+          <button type='submit' className='rounded-full p-2.5 '>
             <Send className='text-primary' />
           </button>
         </form>
