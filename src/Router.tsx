0c29a9700404fd8cd016306bdd6145ac29f640cb
sholikul <sholikulardian@gmail.com>
2024-12-15 13:24:23 +0700

    define path login

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import { App } from "./pages/App";
 import { RegisterPage } from "./pages/Register";
+import { LoginPage } from "./pages/Login";
 
 export const Router = () => {
   return (
     <Routes>
       <Route path='/' element={<App />} />
       <Route path='/register' element={<RegisterPage />} />
+      <Route path='/login' element={<LoginPage />} />
     </Routes>
   );
 };
