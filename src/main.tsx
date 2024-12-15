370b6714ad1b0a9ef4f11095ce3ed8b42ece6a7a
sholikul <sholikulardian@gmail.com>
2024-12-15 11:53:00 +0700

    define browser route

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { App } from "./App.tsx";
 import Layout from "./Layout/Layout.tsx";
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "./Router.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Layout>
-      <App />
-    </Layout>
+    <BrowserRouter>
+      <Layout>
+        <Router />
+      </Layout>
+    </BrowserRouter>
   </StrictMode>
 );
